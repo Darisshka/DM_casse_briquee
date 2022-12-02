37a603b37e48e5347b6fe4eae9e661185a6b758a
Darisshka <117632348+Darisshka@users.noreply.github.com>
2022-12-02 15:27:04 +0100

    Update and rename jeu.py to jeuuu.py

--- jeuuu.py
+++ jeuuu.py
@@ -6,6 +6,9 @@ pyxel.init(128, 128, title="Nuit du c0de")
 
 vaisseau_x = 45
 vaisseau_y = 100
+balle_x = 60
+balle_y = 80
+
 
 def vaisseau_deplacement(x, y):
     """déplacement avec les touches de directions"""
@@ -16,44 +19,39 @@ def vaisseau_deplacement(x, y):
     if pyxel.btn(pyxel.KEY_LEFT):
         if (x > 0) :
             x = x - 1
-   
     return x, y
 
+def balle_deplacement(x, y):
+    if (x < 123):
+        x = x + 1
+        y = y - 1
+    elif (x == 124):
+        if (y > 5):
+            x = x - 1
+            y = y - 1
+    elif (y == 5):
+        return x,y
+     
+    
 
-
-
-    global vaisseau_x, vaisseau_y
+def update():
+    
+    global vaisseau_x, vaisseau_y, balle_x, balle_y
 
     # mise à jour de la position du vaisseau
     vaisseau_x, vaisseau_y = vaisseau_deplacement(vaisseau_x, vaisseau_y)
+    balle_x, balle_y = balle_deplacement(balle_x, balle_y)
     
-    platform_x = 180
-platform_y = 260
-platform_velocity = 10
-platform_color = 5
-platform_sideWidth = 30
-platform_width = 50
-platform_height = 15
-
-# variables linked to the ball 
-ball_x = screen_width / 2
-ball_y = screen_height / 2
-ball_totalVelocity = 5
-ball_velocity_x = (- ball_totalVelocity)
-ball_velocity_y = (- ball_totalVelocity)
-ball_radius = 4
-ball_color = 5
-ball_run = False
-
-
+  
 
 def draw():
     
-
-   
+  
     pyxel.cls(0)
 
-    
-    pyxel.rect(vaisseau_x, vaisseau_y, 35, 5, 5)
+        pyxel.rect(vaisseau_x, vaisseau_y, 35, 5, 5)
+        pyxel.circ(balle_x, balle_y, 2, 3)
 
 pyxel.run(update, draw)
+
+
