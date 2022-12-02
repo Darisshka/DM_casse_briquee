04bf6acf084e8d13980987c4f0025864245b66ff
Darisshka <117632348+Darisshka@users.noreply.github.com>
2022-12-02 14:32:27 +0100

    Update and rename jeuuu1234567.py to jeu.py

--- jeu.py
+++ jeu.py
@@ -1,11 +1,9 @@
 import pyxel
 
-# taille de la fenetre 128x128 pixels
-# ne pas modifier
+
 pyxel.init(128, 128, title="Nuit du c0de")
 
-# position initiale du vaisseau
-# (origine des positions : coin haut gauche)
+
 vaisseau_x = 45
 vaisseau_y = 100
 
@@ -22,28 +20,40 @@ def vaisseau_deplacement(x, y):
     return x, y
 
 
-# =========================================================
-# == UPDATE
-# =========================================================
-def update():
-    """mise à jour des variables (30 fois par seconde)"""
+
 
     global vaisseau_x, vaisseau_y
 
     # mise à jour de la position du vaisseau
     vaisseau_x, vaisseau_y = vaisseau_deplacement(vaisseau_x, vaisseau_y)
+    
+    platform_x = 180
+platform_y = 260
+platform_velocity = 10
+platform_color = 5
+platform_sideWidth = 30
+platform_width = 50
+platform_height = 15
+
+# variables linked to the ball 
+ball_x = screen_width / 2
+ball_y = screen_height / 2
+ball_totalVelocity = 5
+ball_velocity_x = (- ball_totalVelocity)
+ball_velocity_y = (- ball_totalVelocity)
+ball_radius = 4
+ball_color = 5
+ball_run = False
 
 
-# =========================================================
-# == DRAW
-# =========================================================
+
 def draw():
-    """création des objets (30 fois par seconde)"""
+    
 
-    # vide la fenetre
+   
     pyxel.cls(0)
 
-    # vaisseau (carre 8x8)
+    
     pyxel.rect(vaisseau_x, vaisseau_y, 35, 5, 5)
 
 pyxel.run(update, draw)
