47b56766d6ca6843ed8078b40f504d9c51a6a2ca
Darisshka <117632348+Darisshka@users.noreply.github.com>
2022-12-03 11:35:26 +0100

    Update jeuuu.py

--- jeuuu.py
+++ jeuuu.py
@@ -1,24 +1,28 @@
 import pyxel
 
-
+# taille de la fenetre 128x128 pixels
+# ne pas modifier
 pyxel.init(128, 128, title="Nuit du c0de")
 
-
+# position initiale du vaisseau
+# (origine des positions : coin haut gauche)
 vaisseau_x = 45
-vaisseau_y = 100
-balle_x = 60
-balle_y = 80
-
+vaisseau_y = 110
+balle_x = 63
+balle_y = 106
+vies = 3
 
 def vaisseau_deplacement(x, y):
     """déplacement avec les touches de directions"""
-
     if pyxel.btn(pyxel.KEY_RIGHT):
         if (x < 120) :
             x = x + 1
     if pyxel.btn(pyxel.KEY_LEFT):
         if (x > 0) :
             x = x - 1
+
+    
+    
     return x, y
 
 def balle_deplacement(x, y):
@@ -31,27 +35,35 @@ def balle_deplacement(x, y):
             y = y - 1
     elif (y == 5):
         return x,y
-     
-    
+    return x, y 
 
+# =========================================================
+# == UPDATE
+# =========================================================
 def update():
-    
+    """mise à jour des variables (30 fois par seconde)"""
+
     global vaisseau_x, vaisseau_y, balle_x, balle_y
 
     # mise à jour de la position du vaisseau
     vaisseau_x, vaisseau_y = vaisseau_deplacement(vaisseau_x, vaisseau_y)
-    balle_x, balle_y = balle_deplacement(balle_x, balle_y)
+    balle_deplacement_x, balle_deplacement_y = balle_deplacement(balle_x, balle_y)
     
-  
 
+
+# =========================================================
+# == DRAW
+# =========================================================
 def draw():
-    
-  
-    pyxel.cls(0)
+    """création des objets (30 fois par seconde)"""
 
-        pyxel.rect(vaisseau_x, vaisseau_y, 35, 5, 5)
-        pyxel.circ(balle_x, balle_y, 2, 3)
+    # vide la fenetre
+    pyxel.cls(0)
+    
+
+    # vaisseau (carre 8x8)
+    pyxel.rect(vaisseau_x, vaisseau_y, 40, 5, 6)
+    pyxel.circ(balle_x, balle_y, 3, 2)
 
 pyxel.run(update, draw)
-
 
